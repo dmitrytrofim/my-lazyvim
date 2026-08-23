0a03a298d5c61bb49aad54f40825e572aa934fad
dmitrytrofim <trofimov.dmitr2017@yandex.by>
2026-08-23 09:19:00 +0300

    fix anime cursor neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -19,3 +19,7 @@ if vim.g.neovide then
   vim.opt.mousescroll = "ver:7,hor:6"
 end
 
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0 -- Убирает время анимации (курсор перемещается мгновенно)
+  vim.g.neovide_cursor_trail_size = 0        -- Убирает шлейф за курсором
+end
